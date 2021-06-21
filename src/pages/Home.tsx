4ae8bee48df45e071f43a63a37464cf4c4c03f59
alanflp <alan.felipe@querodelivery.com>
2021-06-20 21:49:07 -0300

    feat: projeto finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,28 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if(!newTaskTitle) return null
+
+    const id = new Date().getTime()
+    const newTask = {
+      id,
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const newTasks = [...tasks].map(task => task.id === id ? {...task, done: !task.done} : {...task})
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = [...tasks].filter(task => task.id !== id)
+    setTasks(newTasks)
   }
 
   return (
